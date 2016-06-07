c9f3455c29ce77205b0cbbb7dbde7143394b875a
swfree <skye.free@gmail.com>
2016-06-07 15:03:08 -0700

    Add http service for accessing alchemy api

---
+++ src/app/news-browser.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Article } from './article';
+import { Observable }     from 'rxjs/Observable';
+
+@Injectable()
+export class NewsService {
+  constructor (private http: Http) { }
+
+  private alchemyAPI = 'https://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=b3b388b1b36883fcd5c206682256a6689ec89688&outputMode=json&start=now-7d&end=now&maxResults=10&q.enriched.url.title=yosemite&return=enriched.url.title,enriched.url.text,enriched.url.image,enriched.url.url';
+
+  getNews(): Observable<Article[]> {
+    return this.http.get(this.alchemyAPI)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.result.docs || { };
+  }
+
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
